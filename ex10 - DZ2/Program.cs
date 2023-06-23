de4d5d77e87c7635871725ed85b7ba537ab5751a
Sergey Avdeev <sergeyavdeev@MacBook-Pro-Sergey.local>
2023-06-23 09:16:10 +0300

    сделали домашнее задание

---
+++ ex10 - DZ2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Задача 10: 
+Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+String userInput = Console.ReadLine() ?? ""; 
+int num = int.Parse(userInput);
+
+int a2 = num % 100;
+int res = a2 / 10;
+
+Console.WriteLine($"Вывод второго числа: {num} -> {res}");
---
+++ ex13 - DZ2/Program.cs
@@ -0,0 +1,44 @@
+﻿/* 
+Задача 13: 
+Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Clear();
+
+Console.Write("Введите целое число: ");
+String userInput = Console.ReadLine() ?? ""; 
+int num = int.Parse(userInput);
+
+if (num < 100)
+{
+    Console.WriteLine($"Третьего номера числа {num} нет");
+}
+
+if (num >= 100 && num < 999)
+{
+    int res = num % 10;
+    Console.WriteLine($"Вывод третьего числа: {num} -> {res}");
+}
+
+if (num >= 1000 && num < 9999)
+{
+    int a1 = num % 100;
+    int res = a1 / 10;
+    Console.WriteLine($"Вывод третьего числа: {num} -> {res}");
+}
+
+if (num >= 10000 && num < 99999)
+{
+    int a1 = num % 1000;
+    int res = a1 / 100;
+    Console.WriteLine($"Вывод третьего числа: {num} -> {res}");
+}
+
+if (num >= 100000)
+{
+    Console.WriteLine("Ошибка ввода");
+}
